e828e95df79c150a40b74dccb2e92dc075ede929
Sri2103 <damasriharsha@gmail.com>
2024-04-10 11:01:56 -0400

    add buttons and icons in NavBar

--- internal/ui/views/components/NavBar_templ.go
+++ internal/ui/views/components/NavBar_templ.go
@@ -79,7 +79,7 @@ func NavBar() templ.Component {
 			var templ_7745c5c3_Var4 string
 			templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(getUser(ctx).Name)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `views\components\NavBar.templ`, Line: 54, Col: 33}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `views\components\NavBar.templ`, Line: 61, Col: 34}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
 			if templ_7745c5c3_Err != nil {
